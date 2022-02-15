edd7ec176fe14ca1c690056898452fe261d05133
Richard Shashalevich <richardsasalevic@gmail.com>
2022-02-15 20:42:06 +0200

    I started on thinking how to implement this function, and started coding 	- Added simple, addiong algorithm, have a lot of bags

---
+++ go.mod
@@ -0,0 +1,3 @@
+module longadd
+
+go 1.17
---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+)
+
+// I know there is a better way of doing this
+
+func addZeros(a string, time int) string {
+	for i := time; i > 0; i-- {
+		a = "0" + a
+	}
+	return a
+}
+
+func Normalize(a, b string) (string, string) {
+	if len(a) > len(b) {
+		b = addZeros(b, len(a)-len(b))
+	} else {
+		a = addZeros(a, len(b)-len(a))
+	}
+	return a, b
+}
+
+func LongAdd(a, b string) string {
+	result := ""
+	var add uint8
+
+	for i := len(a) - 1; i >= 0; i-- {
+		num := (a[i] - '0') + (b[i] - '0') + add
+		if num > 9 {
+			add = 1
+		} else {
+			add = 0
+		}
+		result = string((num%10)+'0') + result
+	}
+	if add != 0 {
+		result = LongAdd(b[:len(a)-1], "1") + result
+	}
+	return result
+}
+
+func main() {
+	fmt.Println(LongAdd("12", "93"))
+}
