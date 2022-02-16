0bdc9a27591ce62469ca5da5a47f31a10dfd6603
Sp0ozy <imolchanov210@gmail.com>
2022-02-16 21:31:39 +0200

    bug with 55555 +55 left

--- main.go
+++ main.go
@@ -61,14 +61,62 @@ func Byte2Number(a byte) int {
 func LongFibonacci(n int) string {
 	a, b := "0", "1"
 	var i int
-	for ; i < n-1; i++ {
+	for ; i < n; i++ {
 		a, b = b, LongAdd(a, b)
 
 	}
-	return b
+	return a
+}
+func LongAddEx(a, b string, base int) (s string) {
+	if len(a) < len(b) {
+		a, b = b, a
+	}
+	iB := len(b)
+	var r []rune
+	tmp := 0
+	for i := len(a) - 1; i >= 0; i-- {
+		if iB > 0 {
+			iB--
+			if Byte2Number(a[i])+Byte2Number(b[iB])+tmp > base-1 {
+				r = append(r, rune(Byte2Number(a[i])+Byte2Number(b[iB])-base+tmp))
+				tmp = 1
+
+			} else {
+				r = append(r, rune(Byte2Number(a[i])+Byte2Number(b[iB])+tmp))
+				tmp = 0
+
+			}
+		} else {
+			if Byte2Number(a[i])+tmp > base-1 {
+				r = append(r, 0)
+				r = append(r, 1)
+				tmp = 0
+
+			} else {
+
+				r = append(r, rune(Byte2Number(a[i])+tmp))
+				tmp = 0
+
+			}
+
+		}
+
+	}
+	if tmp != 0 {
+		r = append(r, 1)
+	}
+
+	var r2 []rune
+
+	for i := len(r) - 1; i >= 0; i-- {
+		r2 = append(r2, r[i]+48)
+	}
+	s = string(r2)
+	return s
 }
 func main() {
 	fmt.Println(LongAdd("98765432109876543210987654321098765432109876543210",
 		"22222222222222222222222222222222222222222222222222"))
 	fmt.Println(LongFibonacci(500))
+	fmt.Println(LongAddEx("55555", "55", 10))
 }
--- main_test.go
+++ main_test.go
@@ -25,3 +25,43 @@ func TestLongAdd(t *testing.T) {
 		})
 	}
 }
+
+func TestLongFibonacci(t *testing.T) {
+	for _, tc := range []struct {
+		n    int
+		want string
+	}{
+		{0, "0"},
+		{1, "1"},
+		{10, "55"},
+		{500, "139423224561697880139724382870407283950070256587697307264108962948325571622863290691557658876222521294125"},
+	} {
+		t.Run("", func(t *testing.T) {
+			if got := LongFibonacci(tc.n); got != tc.want {
+				t.Errorf("got = %v, want = %v", got, tc.want)
+			}
+		})
+	}
+}
+func TestLongAddEx(t *testing.T) {
+	for _, tc := range []struct {
+		a    string
+		b    string
+		base int
+		want string
+	}{
+		{"", "", 0, ""},
+		{"0", "0", 1, "0"},
+		{"1", "1", 2, "10"},
+		{"999", "1001", 10, "2000"},
+		{"AB00", "CD", 16, "55610"},
+		{"55555", "55", 10, "55610"},
+		{"123", "2", 5, "130"},
+	} {
+		t.Run("", func(t *testing.T) {
+			if got := LongAddEx(tc.a, tc.b, tc.base); got != tc.want {
+				t.Errorf("got = %v, want = %v", got, tc.want)
+			}
+		})
+	}
+}
