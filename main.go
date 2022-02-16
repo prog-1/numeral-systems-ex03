19aa640758a1acc6e9b5093442dd6e770fad624a
Richard Shashalevvich <richardsasalevic@gmail.com>
2022-02-16 18:16:50 +0200

    Tests for LongFibanachi

--- main.go
+++ main.go
@@ -36,7 +36,29 @@ func LongAdd(a, b string) string {
 	}
 	return result
 }
+
+func LongAdd2(a, b string) string {
+	result := ""
+	var add uint8
+	a, b = Normalize(a, b)
+	for i := len(a) - 1; i >= 0; i-- {
+		num := (a[i] - '0') + (b[i] - '0') + add
+		if num > 9 {
+			add = 1
+		} else {
+			add = 0
+		}
+		result = string((num%10)+'0') + result
+	}
+	if add != 0 {
+		result = "1" + result
+	}
+	return result
+}
 func LongFibonacci(n int) string {
+	if n == 0 {
+		return "0"
+	}
 	prev, now := "0", "1"
 	for i := 1; i < n; i++ {
 		prev, now = now, LongAdd(prev, now)
--- main_test.go
+++ main_test.go
@@ -2,13 +2,15 @@ package main
 
 import "testing"
 
-func TestToBase10(t *testing.T) {
+func TestLongAdd(t *testing.T) {
 	for _, tc := range []struct {
 		name      string
 		a         string
 		b         string
 		sumResult string
 	}{
+
+		{"zero", "0", "0", "0"},
 		{"simple math", "123", "210", "333"},
 		{"Math with memorization ", "55555", "55", "55610"},
 		{"Long number", "98765432109876543210987654321098765432109876543210",
@@ -18,6 +20,22 @@ func TestToBase10(t *testing.T) {
 		t.Run(tc.name, func(t *testing.T) {
 			if got := LongAdd(tc.a, tc.b); got != tc.sumResult {
 				t.Errorf("LongAdd(%v, %v) = %v, want %v", tc.a, tc.b, got, tc.sumResult)
+			}
+		})
+	}
+}
+func TestLongFibonacci(t *testing.T) {
+	for _, tc := range []struct {
+		n      int
+		Result string
+	}{
+		{0, "0"},
+		{10, "55"},
+		{500, "139423224561697880139724382870407283950070256587697307264108962948325571622863290691557658876222521294125"},
+	} {
+		t.Run("Test", func(t *testing.T) {
+			if got := LongFibonacci(tc.n); got != tc.Result {
+				t.Errorf("LongFibonacci(%v) = %v, want %v", tc.n, got, tc.Result)
 			}
 		})
 	}
